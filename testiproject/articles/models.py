a02f342ef6efad9581a5473070ff5f53f0b90f14
Kiviperä Jouni <jouni.kivipera@turku.fi>
2021-09-09 11:47:54 +0300

    muutoksia

--- testiproject/articles/models.py
+++ testiproject/articles/models.py
@@ -3,5 +3,5 @@ from django.db.models.fields import TextField
 
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=40, blank=True)
     content = models.TextField()
